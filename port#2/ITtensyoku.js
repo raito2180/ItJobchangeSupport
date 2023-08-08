127d82de1fac04d8c7ab9e0e37f435080936c55a
Yuta Suzuki <g.uc2180@gmail.com>
2023-08-08 20:50:33 +0900

    Top画面-js
    タイトルとボタンのタイトル反映

---
+++ port#2/ITtensyoku.js
@@ -0,0 +1,17 @@
+//題名を反映
+const ITtitle = "ITエンジニア転職 情報整理リスト";
+document.getElementById("ITtensyoku").textContent = ITtitle;
+
+const btnTitle = ["情報リスト作成","情報リスト管理"];
+const $button = document.getElementsByTagName("button");
+
+//ボタン名を反映
+let btnIndex = 0;
+while (btnIndex < btnTitle.length){
+  $button[btnIndex].textContent = btnTitle[btnIndex];
+  btnIndex++;
+}
+
+
+
+
