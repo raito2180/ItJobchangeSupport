d8534fc49fc7529bee50e47439863c527b72b350
Yuta Suzuki <g.uc2180@gmail.com>
2023-08-08 21:29:23 +0900

    リスト作成画面-4つのボタン-js
    印刷機能の追加

--- port#2/windowRight1.js
+++ port#2/windowRight1.js
@@ -12,12 +12,19 @@ while (btnIndex < btnTitle.length){
 const question = () => {
   window.open("ques.html");
   console.log("ques.html");
-  }
-
-//情報リスト作成を押したときにquestion関数を呼び出す
+}
+//目標再設定を押したときにquestion関数を呼び出す
   $button[0].addEventListener ("click",e => {
     question(e);
   }) 
 
+//クリックしたときにページが印刷される関数を定義
+const listPrint = () => {
+  window.print();
+}
+//印刷を押したときにlistPrint関数を呼び出す
+  $button[1].addEventListener ("click",e => {
+    listPrint(e);
+  }) 
 
 
