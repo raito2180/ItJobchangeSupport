b0253c2c81cdb677872a9bb34da1ec87102407ed
Yuta Suzuki <g.uc2180@gmail.com>
2023-08-08 21:03:26 +0900

    リスト作成画面-右下側画面-js
    リスト作成画面-右下側画面-js

---
+++ port#2/windowRight2.js
@@ -0,0 +1,11 @@
+/*const btnTitle = ["目標再設定","印刷","保存","戻る"];
+const $button = document.getElementsByTagName("button");
+
+//ボタン名を反映
+let btnIndex = 0;
+while (btnIndex < btnTitle.length){
+  $button[btnIndex].textContent = btnTitle[btnIndex];
+  btnIndex++;
+}*/
+
+//クリックしたら別htmlに遷移
