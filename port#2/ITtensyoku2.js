109d1a52e6174b56841c1f1b14ea7fd059ffb0cf
Yuta Suzuki <g.uc2180@gmail.com>
2023-08-12 23:23:34 +0900

    Update ITtensyoku2.js

--- port#2/ITtensyoku2.js
+++ port#2/ITtensyoku2.js
@@ -1,4 +1,4 @@
-const btnTitle = ["目標再設定","印刷","保存","戻る"];
+const btnTitle = ["目標再設定","印刷","保存","戻る","入力"];
 const $button = document.getElementsByTagName("button");
 
 //ボタン名を反映
@@ -8,21 +8,40 @@ while (btnIndex < btnTitle.length){
   btnIndex++;
 }
 
-//ques.htmlを別ウィンドウに呼び出す関数を定義
-const questionJob = () => {
-  window.open("ques.html");
-}
-//目標再設定を押したときにquestion関数を呼び出す
-  $button[0].addEventListener ("click",e => {
-    questionJob(e);
-  }) 
+//
+function getValue(){
+  const checkBusiness = document.getElementsByName("business");
+  console.log(checkBusiness);
+  for(i=0; i<checkBusiness.length; i++){
+  if(checkBusiness[i].checked === true){
+  businessForm.textContent = checkBusiness[i].value;
+  }}
+  const checkJob = document.getElementsByName("job");
+  console.log(checkJob);
+  for(i=0; i<checkJob.length; i++){
+  if(checkJob[i].checked === true){
+  jobForm.textContent = checkJob[i].value;
+  }}
+  }
+//目標再設定を押したときにID:goalを呼び出す
+$button[0].addEventListener ("click",e => {
+  ques.classList.remove("hidden");
+}) 
 
 //印刷を押したときに印刷画面を呼び出す
-  $button[1].addEventListener ("click",e => {
-    window.print(e);
-  }) 
+$button[1].addEventListener ("click",e => {
+  window.print(e);
+}) 
+
+//保存を押した時に情報を保存しておく
 
 //戻るを押したときにTop画面に戻る
-  $button[3].addEventListener ("click",e => {
-  window.history.back(e);
+$button[3].addEventListener ("click",e => {
+window.history.back(e);
+}) 
+
+//入力を押したときにgetValue関数を呼び出す
+  $button[4].addEventListener ("click",e => {
+  getValue(e);
+  ques.classList.add("hidden");
 }) 
