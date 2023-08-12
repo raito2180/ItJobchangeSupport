acaaf01469467deeaf7cd591cc09e08608204235
Yuta Suzuki <g.uc2180@gmail.com>
2023-08-12 19:53:19 +0900

    Update ques.js

--- port#2/ques.js
+++ port#2/ques.js
@@ -1,13 +1,19 @@
 const buttonTitle = "入力";
 document.getElementById("job").textContent = buttonTitle;
 
-const checkJob = document.getElementsByTagName("input");
-window.addEventListener("change",checkboxStatus,false); 
 
-function checkboxStatus(){
-if (this.checked){
-  jobForm.textContent = document.getElementsByTagName("input").value;
-}else{
-  jobForm.textContent = "";
-}
+
+function getValue(){
+const checkBusiness = document.getElementsByName("business");
+console.log(checkBusiness);
+for(i=0; i<checkBusiness.length; i++){
+if(checkBusiness[i].checked === true){
+businessForm.textContent = checkBusiness[i].value;
+}}
+const checkJob = document.getElementsByName("job");
+console.log(checkJob);
+for(i=0; i<checkJob.length; i++){
+if(checkJob[i].checked === true){
+jobForm.textContent = checkJob[i].value;
+}}
 }
